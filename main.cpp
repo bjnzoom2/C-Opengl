e46c355bfae164109fe51e6c8b509f2e09fce72a
bjnzoom2 <bjnzoom2@gmail.com>
2025-10-12 17:04:04 +0800

    Learnt to create a simple window with OpenGL

--- main.cpp
+++ main.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
+
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
 int main()
 {
-    std::cout << "Hello world!" << '\n';
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Test window", NULL, NULL);
+
+	if (window == NULL) {
+		std::cout << "WINDOW FAILED\n";
+		glfwTerminate();
+		return -1;
+	}
+	else {
+		std::cout << "WINDOW SUCCESS\n";
+	}
+
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+
+	glViewport(0, 0, 800, 600);
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glfwSwapBuffers(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 
     return 0;
 }

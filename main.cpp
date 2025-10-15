581c837db9bcaa8a31a2f8f8e2155228688e2231
bjnzoom2 <bjnzoom2@gmail.com>
2025-10-15 18:43:16 +0800

    Made an RGB triangle

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -9,36 +10,21 @@ enum Color {Red = 0, Green = 1, Blue = 2};
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec4 color;\n"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos, 1.0);\n"
+"   color = vec4(aColor, 1.0);\n"
 "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
-"uniform vec4 color;\n"
+"in vec4 color;\n"
 "void main()\n"
 "{\n"
 "   FragColor = color;\n"
 "}\0";
-
-void colorShift(GLuint shaderProgram, Color color) {
-	float timeValue = glfwGetTime();
-	float colorValue = sin(timeValue) / 2 + 0.5f;
-	GLint colorUniformLocation = glGetUniformLocation(shaderProgram, "color");
-
-	switch (color) {
-		case 0:
-			glUniform4f(colorUniformLocation, colorValue, 0.0f, 0.0f, 0.0f);
-			break;
-		case 1:
-			glUniform4f(colorUniformLocation, 0.0f, colorValue, 0.0f, 0.0f);
-			break;
-		case 2:
-			glUniform4f(colorUniformLocation, 0.0f, 0.0f, colorValue, 0.0f);
-			break;
-	}
-}
 
 int main()
 {
@@ -48,8 +34,8 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	int width = 800;
-	int height = 800;
+	unsigned int width = 800;
+	unsigned int height = 800;
 
 	GLFWwindow* window = glfwCreateWindow(width, height, "Test window", NULL, NULL);
 
@@ -63,9 +49,9 @@ int main()
 	}
 
 	GLfloat vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
+		-0.5f, -0.5f, 0.0f,    1.0f, 0.0f, 0.0f,
+		0.5f, -0.5f, 0.0f,    0.0f, 1.0f, 0.0f,
+		0.0f, 0.5f, 0.0f,    0.0f, 0.0f, 1.0f
 	};
 
 	glfwMakeContextCurrent(window);
@@ -99,8 +85,11 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	PolyMode mode = Fill;
 
@@ -118,10 +107,9 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
-		colorShift(shaderProgram, Blue);
-
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices) / sizeof(vertices[0]));
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
